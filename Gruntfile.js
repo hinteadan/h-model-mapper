9a99e85085b17b7f173d258d35cdca0a40472b4a
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-01-21 10:57:06 +0200

    Configure grunt with jshint

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'src/{,*/}*.js'
+            ]
+        }
+    });
+
+    grunt.registerTask('default', [
+        'jshint'
+    ]);
+};
